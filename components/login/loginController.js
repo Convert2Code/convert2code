b53281b942a876b3db5fe1c011c028b76a22eded
Caleb Weaver <caleb@benefix.us>
2018-11-22 10:25:13 -0500

    build out login and register stubs

---
+++ components/login/loginController.js
@@ -0,0 +1,10 @@
+'use strict';
+
+convert.controller('loginController', ['$scope', '$resource', '$rootScope', '$http', '$location',
+  function ($scope, $resource, $rootScope, $http, $location) {
+
+    $scope.login = function() {
+      // TODO Login logic here
+    }
+
+  }]);
---
+++ components/register/registerController.js
@@ -0,0 +1,15 @@
+'use strict';
+
+convert.controller('registerController', ['$scope', '$resource', '$rootScope', '$http', '$location',
+  function ($scope, $resource, $rootScope, $http, $location) {
+
+  	// Need to retrieve all usernames to validate in browser, sans server
+  	$scope.usernames = [];
+  	// Need to retrieve all active tech tags from server to select interests
+  	$scope.tags = [];
+
+    $scope.register = function() {
+      // TODO Login logic here
+    }
+
+  }]);
